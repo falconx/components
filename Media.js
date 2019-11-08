dc10559eee1f326fbd301ff89e2cc9f38aa50c05
Matt Layton <mattlayt@gmail.com>
2019-11-08 20:06:24 +0000

    remove unused variable

--- Media.js
+++ Media.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import map from 'lodash/map';
 import debounce from 'lodash/debounce';
 
