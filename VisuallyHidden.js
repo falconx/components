fba8a1663aa286a5c42a15163df5ebc27551ee22
Matt Layton <mattlayt@gmail.com>
2020-04-04 15:24:01 +0100

    refactor: update visually hidden styles

--- VisuallyHidden.js
+++ VisuallyHidden.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 
 const VisuallyHidden = styled.div`
   position: absolute;
-  left: -999vw;
+  clip: rect(1px, 1px, 1px, 1px);
+  padding: 0;
+  border: 0;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
 `;
 
 export default VisuallyHidden;
