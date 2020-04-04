f29ec1a2fbf92007134e25cfb2001e504834694b
Matt Layton <mattlayt@gmail.com>
2020-04-04 15:29:01 +0100

    fix: update VerticalSpacing to avoid collapsing margins

--- Spacer.js
+++ Spacer.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const SPACING_UNITS = 10;
 
 const Spacer = styled.div`
-  margin-bottom: ${props => props.size * SPACING_UNITS}px;
+  height: ${props => props.size * SPACING_UNITS}px;
 `;
 
 Spacer.propTypes = {
