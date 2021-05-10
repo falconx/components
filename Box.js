b3d29fe35c10ff2cc079ab448e8246c6b5de5538
Matt Layton <mattlayt@gmail.com>
2021-05-10 12:51:30 +0100

    Update Box.js

--- Box.js
+++ Box.js
@@ -5,15 +5,15 @@ import shouldForwardProp from '@styled-system/should-forward-prop';
 
 // https://styled-system.com/guides/build-a-box
 const Box = styled('div', { shouldForwardProp })`
+  // ensures the Box can shrink below its minimum content size when used as a flex item
+  min-width: 0;
+
   ${compose(
     space,
     color,
     layout,
     flexbox
   )}
-
-  // ensures the Box can shrink below its minimum content size when used as a flex item
-  min-width: 0;
 `;
 
 Box.propTypes = {
