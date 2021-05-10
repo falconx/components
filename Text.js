703dd928ba3af4508d485111b6557465f14ab159
Matt Layton <mattlayt@gmail.com>
2021-05-10 12:53:33 +0100

    Update Text.js

--- Text.js
+++ Text.js
@@ -1,10 +1,16 @@
 import styled from '@emotion/styled';
-import { compose, color, typography } from 'styled-system';
+import { system, compose, color, typography } from 'styled-system';
 import { createPropTypes } from '@styled-system/prop-types';
 import shouldForwardProp from '@styled-system/should-forward-prop';
 
 const Text = styled('div', { shouldForwardProp })`
   ${compose(color, typography)}
+
+  ${system({
+    textTransform: true,
+    textDecoration: true,
+    whiteSpace: true,
+  })}
 `;
 
 Text.propTypes = {
